57071ba7381c241d61b461c27f7a9996e56106a4
Tobias Reuter <tobias.reuter@rocket-internet.de>
2016-05-17 12:15:15 +0200

    DRY violation example Amenities - branching

--- DRY/Amenities/AmenitiesBuilder.php
+++ DRY/Amenities/AmenitiesBuilder.php
@@ -28,4 +28,28 @@ class AmenitiesBuilder
             'leisure' => $leisure,
         ];
     }
+
+    public function versionOneBranched($request)
+    {
+        // other code left out
+
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities);
+            $amenities = explode(',', $amenities);
+        }
+        $leisure = $request->getQuery('leisure', [], []);
+        if (!empty($leisure) && !is_array($leisure)) {
+            $leisure = htmlspecialchars($leisure);
+            $leisure = explode(',', $leisure);
+        }
+
+        // other code left out
+
+        return [
+            'amenities' => $amenities,
+            'leisure' => $leisure,
+        ];
+    }
+
 }
