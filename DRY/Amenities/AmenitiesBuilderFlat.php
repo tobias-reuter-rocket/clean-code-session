5e2e2d8618d684a37cad98ecd6d477e0b338a30b
Tobias Reuter <tobias.reuter@rocket-internet.de>
2016-05-17 12:23:28 +0200

    DRY violation example Amenities - full flat version

---
+++ DRY/Amenities/AmenitiesBuilderFlat.php
@@ -0,0 +1,128 @@
+<?php
+
+
+namespace CleanCode\DRY;
+
+
+class AmenitiesBuilderFlat
+{
+
+    public function versionOne($request)
+    {
+        // other code left out
+
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities);
+            $amenities = explode(',', $amenities);
+        }
+        $leisure = $request->getQuery('leisure', [], []);
+        if (!empty($leisure) && !is_array($leisure)) {
+            $leisure = htmlspecialchars($leisure);
+            $leisure = explode(',', $leisure);
+        }
+
+        // other code left out
+
+        return [
+            'amenities' => $amenities,
+            'leisure' => $leisure,
+        ];
+    }
+
+
+    public function versionTwoByDeveloperA($request)
+    {
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities, ENT_HTML5);
+            $amenities = explode(',', $amenities);
+        }
+        $leisure = $request->getQuery('leisure', [], []);
+        if (!empty($leisure) && !is_array($leisure)) {
+            $leisure = htmlspecialchars($leisure, ENT_HTML5);
+            $leisure = explode(',', $leisure);
+        }
+
+        return [
+            'amenities' => $amenities,
+            'leisure' => $leisure,
+        ];
+    }
+
+
+    public function versionTwoByDeveloperB($request)
+    {
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities);
+            $amenities = explode(',', $amenities);
+        }
+        $leisure = $request->getQuery('leisure', [], []);
+        if (!empty($leisure) && !is_array($leisure)) {
+            $leisure = htmlspecialchars($leisure);
+            $leisure = explode(',', $leisure);
+        }
+        $utilities = $request->getQuery('utilities', [], []);
+        if (!empty($utilities) && !is_array($utilities)) {
+            $utilities = htmlspecialchars($utilities);
+            $utilities = explode(',', $utilities);
+        }
+        $rules = $request->getQuery('rules', [], []);
+        if (!empty($rules) && !is_array($rules)) {
+            $rules = htmlspecialchars($rules);
+            $rules = explode(',', $rules);
+        }
+        $activities = $request->getQuery('activities', [], []);
+        if (!empty($activities) && !is_array($activities)) {
+            $activities = htmlspecialchars($activities);
+            $activities = explode(',', $activities);
+        }
+
+        return [
+            'amenities' => $amenities,
+            'leisure' => $leisure,
+            'utilities' => $utilities,
+            'rules' => $rules,
+            'activities' => $activities,
+        ];
+    }
+
+
+    public function versionThreeMerged($request)
+    {
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities, ENT_HTML5);
+            $amenities = explode(',', $amenities);
+        }
+        $leisure = $request->getQuery('leisure', [], []);
+        if (!empty($leisure) && !is_array($leisure)) {
+            $leisure = htmlspecialchars($leisure, ENT_HTML5);
+            $leisure = explode(',', $leisure);
+        }
+        $utilities = $request->getQuery('utilities', [], []);
+        if (!empty($utilities) && !is_array($utilities)) {
+            $utilities = htmlspecialchars($utilities);
+            $utilities = explode(',', $utilities);
+        }
+        $rules = $request->getQuery('rules', [], []);
+        if (!empty($rules) && !is_array($rules)) {
+            $rules = htmlspecialchars($rules);
+            $rules = explode(',', $rules);
+        }
+        $activities = $request->getQuery('activities', [], []);
+        if (!empty($activities) && !is_array($activities)) {
+            $activities = htmlspecialchars($activities);
+            $activities = explode(',', $activities);
+        }
+
+        return [
+            'amenities' => $amenities,
+            'leisure' => $leisure,
+            'utilities' => $utilities,
+            'rules' => $rules,
+            'activities' => $activities,
+        ];
+    }
+}
