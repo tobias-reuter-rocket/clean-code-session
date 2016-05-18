ab47125b3ca12a3708ca8f23bc1da04fe5e1775d
Tobias Reuter <tobias.reuter@rocket-internet.de>
2016-05-18 11:37:20 +0200

    DRY Amenities / refactoring solution branch - extract method refactoring

--- DRY/Amenities/AmenitiesBuilderRefactoringLiveDemo.php
+++ DRY/Amenities/AmenitiesBuilderRefactoringLiveDemo.php
@@ -10,11 +10,7 @@ class AmenitiesBuilder
     {
         // other code left out
 
-        $amenities = $request->getQuery('amenities', [], []);
-        if (!empty($amenities) && !is_array($amenities)) {
-            $amenities = htmlspecialchars($amenities);
-            $amenities = explode(',', $amenities);
-        }
+        $amenities = $this->getAmenity($request);
         $leisure = $request->getQuery('leisure', [], []);
         if (!empty($leisure) && !is_array($leisure)) {
             $leisure = htmlspecialchars($leisure);
@@ -27,5 +23,20 @@ class AmenitiesBuilder
             'amenities' => $amenities,
             'leisure' => $leisure,
         ];
+    }
+
+    /**
+     * @param $request
+     * @return array|string
+     */
+    private function getAmenity($request)
+    {
+        $amenities = $request->getQuery('amenities', [], []);
+        if (!empty($amenities) && !is_array($amenities)) {
+            $amenities = htmlspecialchars($amenities);
+            $amenities = explode(',', $amenities);
+        }
+
+        return $amenities;
     }
 }
